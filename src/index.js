c32562a70918b49c818c5863fb2edbdcd2680df9
Faith Ege <faithege@hotmail.co.uk>
2021-09-26 13:14:05 +0100

    Add unit tests and function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-export const projectName = () => {
-  return 'Kata starter project';
+export const statsCalculator = (sequence) => {
+  console.log(`statsCalculator:: ${sequence}`)
+  const minimum = Math.min(...sequence)
+  const maximum = Math.max(...sequence)
+  const length = sequence.length
+
+  const average = sequence.reduce((total, currentValue) => total + currentValue)/length
+  return {
+    minimum,
+    maximum,
+    length,
+    average
+  }
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
-import {projectName} from "./index";
+import {statsCalculator} from "./index";
 
-describe('Starter project should', () => {
-  it('output its name', () => {
-    expect(projectName()).toEqual('Kata starter project');
+const INPUT_ARRAY = [6, 9, 15, -2, 92, 11]
+
+describe('Stats calculator should', () => {
+  it('return the lowest number in the sequence', () => {
+    expect(statsCalculator(INPUT_ARRAY).minimum).toEqual(-2);
+  });
+
+  it('return the highest number in the sequence', () => {
+    expect(statsCalculator(INPUT_ARRAY).maximum).toEqual(92);
+  });
+
+  it('return the number of elements in the sequence', () => {
+    expect(statsCalculator(INPUT_ARRAY).length).toEqual(6);
+  });
+
+  it('return the average value of the elements in the sequence', () => {
+    // better to use .toBeCloseTo when handling floats so as to avoid failing tests due to rounding errors
+    expect(statsCalculator(INPUT_ARRAY).average).toBeCloseTo(131/6);
   });
 });
